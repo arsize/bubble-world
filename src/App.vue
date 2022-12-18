c4bf326fc8c6c236b92eb067df3e405eef218e89
arsize <arsizeof3416@gmail.com>
2022-12-18 20:19:31 +0800

    [core]项目构建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="ceshi">
+    <div class="nei">nei</div>
+  </div>
+  <RouterView />
+</template>
+
+<style lang="less" scoped>
+.ceshi{
+  .nei{
+    color: red;
+  }
+}
+</style>
---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "home",
+  setup(props, ctx) {
+    return () => (
+      <>
+        <div>hello2</div>
+      </>
+    )
+  },
+})
