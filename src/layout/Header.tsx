a59af2e9fd3f6f8c5f967ed9f370bf0be1d27999
arsize <arsizeof3416@gmail.com>
2022-12-19 17:58:48 +0800

    [feat]Add header components

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,7 +6,23 @@ export default defineComponent({
   setup(props, ctx) {
     return () => (
       <>
-        <div class={style.container}></div>
+        <div class={style.container}>
+          <div class={style.widthLimit}>
+            <div class={style.logo}>LOGO</div>
+
+            <div class={style.centerBar}>
+              <div>首页</div>
+              <div>文库</div>
+            </div>
+
+            <div class={style.btnBox}>
+              <div>Aa</div>
+              <div>登录</div>
+              <div>注册</div>
+              <div>写文章</div>
+            </div>
+          </div>
+        </div>
       </>
     )
   },
