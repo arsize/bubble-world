4aeba255ffefed63cb081c6eb6c8f59b1af46c4e
arsize <arsizeof3416@gmail.com>
2023-03-06 09:18:06 +0800

    fix: global less & footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,15 +4,10 @@ function Footer() {
   return (
     <Foot>
       <p>关于世界·创作指南·隐私保护指引</p>
-      <br />
       <p>举报邮箱：jubao@gmail.com</p>
-      <br />
       <p>联系我们 © 2023 滴水书屋</p>
-      <br />
       <p>世界是天空中落下的一滴水</p>
-      <br />
       <p>希望它终能变成海洋</p>
-      <br />
     </Foot>
   )
 }
